b0fcc8c7c145efeaacde904d965dc2ce8ed39749
Gavin Keech <gavin.keech@gmail.com>
2013-07-27 20:17:21 +1000

    Added variables of relative freedoms to elementals

--- entpm/structure.js
+++ entpm/structure.js
@@ -91,35 +91,45 @@ ENTPM.STRUCTURE = {
 			minima: "cybernetic",
 			x: "animal",
 			y: "human",
-			z: "robotic"
+			z: "robotic",
+			a: "soft", // to shape
+			b: "firm"  // to shape
 		},
 		"composition": {
 			maxima: "synthetic",
 			minima: "raw",
 			x: "artificial",
 			y: "natural",
-			z: "processed"
+			z: "processed",
+			a: "ignored", // to communication
+			b: "enhanced" // to communication
 		},
 		"shape": {
 			maxima: "loose",
 			minima: "dense",
 			x: "blobs",
 			y: "nets",
-			z: "spikes"
+			z: "spikes",
+			a: "micro", // to environment
+			b: "macro"  // to environment
 		},
 		"communication": {
 			maxima: "precise",
 			minima: "abstract",
 			x: "numbers",
 			y: "words",
-			z: "forms"
+			z: "forms",
+			a: "complex", // to organism
+			b: "simple"   // to organism
 		},
 		"environment": {
 			maxima: "dream",
 			minima: "reality",
 			x: "earth",
 			y: "space",
-			z: "mind"
+			z: "mind",
+			a: "illusory", // to composition
+			b: "real"      // to composition
 		},
 	//  content substrate
 		"identifier": {
@@ -127,35 +137,45 @@ ENTPM.STRUCTURE = {
 			minima: "ego",
 			x: "collective",
 			y: "individual",
-			z: "output"
+			z: "output",
+			a: "altruistic", // to architecture
+			b: "self aware"  // to architecture
 		},
 		"source": {
 			maxima: "culture",
 			minima: "imagination",
 			x: "literature",
 			y: "art",
-			z: "games"
+			z: "games",
+			a: "amplified", // to creation
+			b: "utilised"   // to creation
 		},
 		"architecture": {
 			maxima: "informational",
 			minima: "experiential",
 			x: "distributed",
 			y: "consolidated",
-			z: "biological"
+			z: "biological",
+			a: "complex", // to dimension
+			b: "simple"   // to dimension
 		},
 		"creation": {
 			maxima: "implementation",
 			minima: "idea",
 			x: "work",
 			y: "play",
-			z: "life"
+			z: "life",
+			a: "fun",      // to identifier
+			b: "emotional" // to identifier
 		},
 		"dimension": {
 			maxima: "actual",
 			minima: "virtual",
 			x: "physicality",
 			y: "technology",
-			z: "mind"
+			z: "mind",
+			a: "sensed",  // to source
+			b: "embodied" // to source
 		},
 	//  data substrate
 		"notifier": {
@@ -163,35 +183,45 @@ ENTPM.STRUCTURE = {
 			minima: "off",
 			x: "meaning",
 			y: "language",
-			z: "context"
+			z: "context",
+			a: "understood",   // to temperature
+			b: "misunderstood" // to temperature
 		},
 		"patterns": {
 			maxima: "phases",
 			minima: "cycles",
 			x: "choice",
 			y: "chance",
-			z: "result"
+			z: "result",
+			a: "error",     // to connections
+			b: "correction" // to connections
 		},
 		"temperature": {
 			maxima: "cold",
 			minima: "hot",
 			x: "order",
 			y: "chaos",
-			z: "uncertainty"
+			z: "uncertainty",
+			a: "witnessed", // to hyperconscious
+			b: "neglected"  // to hyperconscious
 		},
 		"connections": {
 			maxima: "macro",
 			minima: "micro",
 			x: "modes",
 			y: "positions",
-			z: "statistics"
+			z: "statistics",
+			a: "stated",  // to notifier
+			b: "remarked" // to notifier
 		},
 		"hyperconscious": {
 			maxima: "subconscious",
 			minima: "conscious",
 			x: "non-presence",
 			y: "presence",
-			z: "emergence"
+			z: "emergence",
+			a: "thought",   // to patterns
+			b: "remembered" // to patterns
 		},
 	//  experience substrate
 		"embodied": {
@@ -199,35 +229,45 @@ ENTPM.STRUCTURE = {
 			minima: "full",
 			x: "amplification",
 			y: "tolerance",
-			z: "decayed"
+			z: "decayed",
+			a: "controlled", // to flowing
+			b: "embraced"    // to flowing
 		},
 		"textural": {
 			maxima: "coarse",
 			minima: "smooth",
 			x: "dreamed",
 			y: "lived",
-			z: "remembered"
+			z: "remembered",
+			a: "encouraged", // to profound
+			b: "distorted"   // to profound
 		},
 		"flowing": {
 			maxima: "paced",
 			minima: "delayed",
 			x: "mystery",
 			y: "discovered",
-			z: "struggle"
+			z: "struggle",
+			a: "managed", // to superficial
+			b: "freed"    // to superficial
 		},
 		"profound": {
 			maxima: "wide",
 			minima: "deep",
 			x: "emotion",
 			y: "aesthetics",
-			z: "sensation"
+			z: "sensation",
+			a: "satisfied", // to embodied
+			b: "insatiable" // to embodied
 		},
 		"superficial": {
 			maxima: "narrow",
 			minima: "high",
 			x: "transformed",
 			y: "delivered",
-			z: "alluded"
+			z: "alluded",
+			a: "instigated", // to textural
+			b: "projected"   // to textural
 		}
 	}
 };
